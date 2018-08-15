e2f7b17e4a7264cf437914a1ee9eb64484fc6695
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-08-15 23:18:39 +0200

    Minor changes.

--- snip/snippet.py
+++ snip/snippet.py
@@ -2,13 +2,6 @@ import yaml
 import re
 
 
-def parse_snippet(file_content):
-    splitter = re.compile(r'^-{3}[\s\S]', re.MULTILINE)
-    _, metadata_content, snippet_content = splitter.split(file_content, 2)
-    metadata = yaml.load(metadata_content)
-    return Snippet(metadata, snippet_content)
-
-
 class Snippet:
     @property
     def metadata(self):
@@ -18,6 +11,13 @@ class Snippet:
     def snippet(self):
         return self._snippet
 
-    def __init__(self, metadata, snippet):
+    def __init__(self, metadata: str, snippet: str):
         self._metadata = metadata
         self._snippet = snippet
+
+
+def parse_snippet(file_content: str) -> Snippet:
+    splitter = re.compile(r'^-{3}[\s\S]', re.MULTILINE)
+    _, metadata_content, snippet_content = splitter.split(file_content, 2)
+    metadata = yaml.load(metadata_content)
+    return Snippet(metadata, snippet_content)
