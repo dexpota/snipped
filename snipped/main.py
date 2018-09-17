2f57fa76a210575690960c234878a943704f7916
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-09-17 22:47:54 +0200

    Disabled debugging logging functionality by default.

--- snipped/main.py
+++ snipped/main.py
@@ -1,14 +1,15 @@
 """snip.
 
 Usage:
-    snip <snippet>
-    snip --info <snippet>
-    snip --edit <snippet>
-    snip --list
-    snip --version|-v
+    snip [options] <snippet>
+    snip --info [options] <snippet>
+    snip --edit [options] <snippet>
+    snip --list [options]
+    snip --version
 
 Options:
-    -v, --version  Show snipped version.
+    --version  Show snipped version.
+    -d --debug  Enable debugging mode.
 
 """
 from .commands import edit_snippet, snippet_info, show_snippet, list_snippets
@@ -19,17 +20,24 @@ import logging
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-
     arguments = docopt(__doc__)
-    logging.debug("arguments: {}".format(arguments))
+
+    if arguments["--debug"]:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.WARNING
+
+    logging.basicConfig(level=logging_level)
 
     repository = LocalRepository()
+
+    logging.debug("arguments: {}".format(arguments))
 
     if arguments["--version"]:
         # print program version
         print("Snipped version {}.".format(__version__))
     elif arguments["--list"]:
+        logging.debug("Listing")
         # list all snippets
         list_snippets(repository)
     elif arguments["--info"]:
