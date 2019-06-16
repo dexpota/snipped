688c9865dbcd22c745c940b501a111b9f712e297
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-06-16 12:04:20 +0200

    Updated github test.

--- tests/test_github.py
+++ tests/test_github.py
@@ -4,11 +4,11 @@ from snipped.repository.github import GithubRepository, RateLimitReached
 def test_github():
     try:
         repository = GithubRepository("dexpota", ".snippets")
+
+        for snippet_id in repository.list():
+            print(snippet_id)
+
+        repository.get_snippet("java/javadoc-method")
     except RateLimitReached as r:
         # When we reach the limit there is nothing we can do except tell the user and wait.
         return
-
-    for snippet_id in repository.list():
-        print(snippet_id)
-
-    repository.get_snippet("java/javadoc-method")
